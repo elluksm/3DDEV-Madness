42dc194e3d543866d6b37571ae1ce94b7b40f406
elluksm <elluksm@gmail.com>
2019-04-10 22:15:53 +0300

    refresh project structure

---
+++ Assets/Scripts/PlayerCollision.cs
@@ -0,0 +1,14 @@
+﻿
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    void OnCollisionEnter (Collision collisionInfo )
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            Debug.Log(collisionInfo.collider.name);
+        }
+
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float forwardForce = 400f;
+    public float sidewaysForce = 100f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate ()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
